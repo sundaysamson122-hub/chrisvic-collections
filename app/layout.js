9a67670248bd5f261aca9b958d9a2f59c5bb75a2
sundaysamson122-hub <sundaysamson122@gmail.com>
2026-08-27 23:53:58 +0100

    Refactor RootLayout to include AuthGuard
    Removed metadata export and added AuthGuard component.

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,15 @@
-export const metadata = {
-  title: 'Chrisvic Collections',
-  description: 'Luxury Fashion & Collections Storefront',
-}
+'use client';
+
+import AuthGuard from '../components/AuthGuard';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, fontFamily: 'sans-serif', background: '#0a0a0a', color: '#fff' }}>
-        {children}
+      <body style={{ margin: 0, fontFamily: 'sans-serif' }}>
+        <AuthGuard>
+          {children}
+        </AuthGuard>
       </body>
     </html>
-  )
+  );
 }
