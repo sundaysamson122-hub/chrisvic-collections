caa3cfea2c81836f62acc46b761497da083446c7
sundaysamson122-hub <sundaysamson122@gmail.com>
2026-09-05 12:43:16 +0100

    Load the storefront only in the browser

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import App from "../artifacts/adodo-collections/src/App";
+import dynamic from "next/dynamic";
+
+const App = dynamic(() => import("../artifacts/adodo-collections/src/App"), {
+  ssr: false,
+});
 
 export default function Page() {
   return <App />;
